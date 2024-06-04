1bf07063ac274b536945ffe0526eb0b8df695fa7
Finkch <skyler@thealdersons.org>
2024-06-04 15:28:08 -0700

    Added skeleton for main

---
+++ main.py
@@ -0,0 +1,11 @@
+# Welcome to PiByPeople!
+
+# This is the main file. It get the ball rolling!
+
+def main():
+    pass
+
+
+# Ensures only the startup thread runs main
+if __name__ == '__main__':
+    main()
