cadee3d4d1bfa95114a39d226c20bc3b4030c305
Finkch <skyler@thealdersons.org>
2024-06-04 16:34:12 -0700

    Added a file/function to read data

---
+++ read.py
@@ -0,0 +1,20 @@
+# Reads a data file, returning the data as a numpy array
+
+from numpy import array
+
+def read(file: str) -> list[int | str]:
+    data = []
+    with open(file, 'r') as f:
+        for line in f:
+
+            # Gets each item
+            splits = line.split(',', 4)
+
+            # Converts first two items to integers
+            splits[0] = int(splits[0])
+            splits[1] = int(splits[1])
+
+            # Adds the data to the arrau
+            data.append(array(splits))
+
+    return array(data)
