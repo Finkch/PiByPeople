1cac6912ab21b8436c9569d058d6a12f1ac83896
Finkch <skyler@thealdersons.org>
2024-06-05 13:21:01 -0700

    Added function to show to comapre histogram to normal distribution

--- show.py
+++ show.py
@@ -1,6 +1,10 @@
 # Shows the results
 
-from math import pi
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import pi
+
+from distribution import Distribution
 
 
 # Returns `π ± unc` with some formatting
@@ -39,6 +43,33 @@ def find_rounding(error: float) -> int:
 
     return i + 1
 
+
+def plot_distribution(distribution: Distribution):
+
+    # Produces a histogram from the random trials
+    y, x = distribution.histogram()
+    x = x[:len(y)]
+    y = y / distribution.trials
+    bins = plt.bar(x, y, 0.09)
+
+    # Compares to a perfect normal distribution
+    plt.plot(*produce_normal(pi, 1))
+
+    plt.show()
+
+
+# A normal distribution
+def normal(x: float, mean: float, std: float) -> list[float]:
+    prefactor = 1 / std / np.sqrt(2 * pi)
+    
+    return prefactor * np.e ** (-0.5 * ((x - mean) / std) ** 2)
+
+# Returns a pair of lists that represent a normal distribution
+def produce_normal(mean: float, std: float):
+    x = np.arange(pi - 2 * std, pi + 2 * std, 0.01)
+    y = normal(x, mean, std)
+
+    return x, y
 
 
 
