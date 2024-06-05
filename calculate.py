9434d96934a899f7e3c05a119319f6de44f158bc
Finkch <skyler@thealdersons.org>
2024-06-05 10:09:36 -0700

    Updated calculate.py to work with PairList

--- calculate.py
+++ calculate.py
@@ -1,22 +1,17 @@
 # Does some calculations
 
 from math import sqrt
+from dataset import PairList
 
 
 # Calculates pi.
 #   The chance a random pair of integers are coprime
 #   to one another goes as 6 / π^2. Solving for π,
 #   then π = sqrt(6 / coprime_fraction)
-def find_pi(nums: list) -> float:
-
-    # Gets the number of coprime pairs in the list of nums
-    coprimes = sum([pair.coprime for pair in nums])
-
-    # Gets the total amount of pairs in the list
-    entries = len(nums)
+def find_pi(pairs: PairList) -> float:
 
     # Calculates the fraction of coprimes in the list
-    coprime_fraction = coprimes / entries
+    coprime_fraction = pairs.coprimes / len(pairs)
 
     # Finds pi
     pi = sqrt(6 / coprime_fraction)
