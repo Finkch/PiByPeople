a26b94c1bc9d2ba53bbb71f58decd6ca373002b2
Finkch <skyler@thealdersons.org>
2024-06-06 14:57:04 -0700

    Refactored show.py for SmallDistribution testing

--- show.py
+++ show.py
@@ -3,8 +3,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy import pi
+from statistics import stdev
 
-from distribution import Distribution
+from distribution import SmallDistribution
+
+from logger import logger
 
 
 # Returns `π ± unc` with some formatting
@@ -44,32 +47,48 @@ def find_rounding(error: float) -> int:
     return i + 1
 
 
-def plot_distribution(distribution: Distribution):
+def plot_distribution(distribution: SmallDistribution):
+
+
+    sd = get_sd(distribution.distribution, pi)
 
     # Produces a histogram from the random trials
-    y, x = distribution.histogram()
-    x = x[:len(y)]
-    y = y / distribution.trials
-    bins = plt.bar(x, y, 0.09)
+    x, y, bin_size = distribution.histogram()
+
+    nx, ny = produce_normal(pi, sd, bin_size / 10)
+
+    y = y / max(y) * max(ny)
+
+
+    bins = plt.bar(x, y, bin_size * 0.9)
+
+    logger.log('post trim', [str(datum) for datum in x] + ['\n\n'] + [str(datum) for datum in y] + ['\n\n'] + [str(sum(y))])
+
+
+    print(sd)
 
     # Compares to a perfect normal distribution
-    plt.plot(*produce_normal(pi, 1))
+    plt.plot(nx, ny)
 
     plt.show()
 
 
 # A normal distribution
-def normal(x: float, mean: float, std: float) -> list[float]:
-    prefactor = 1 / std / np.sqrt(2 * pi)
+def normal(x: float, mean: float, sd: float) -> list[float]:
+    prefactor = 1 / sd / np.sqrt(2 * pi)
     
-    return prefactor * np.e ** (-0.5 * ((x - mean) / std) ** 2)
+    return prefactor * np.e ** (-0.5 * ((x - mean) / sd) ** 2)
 
 # Returns a pair of lists that represent a normal distribution
-def produce_normal(mean: float, std: float):
-    x = np.arange(pi - 2 * std, pi + 2 * std, 0.01)
-    y = normal(x, mean, std)
+def produce_normal(mean: float, sd: float, step_size: float = 0.01):
+    x = np.arange(pi - 2 * sd, pi + 2 * sd, step_size)
+    y = normal(x, mean, sd)
 
     return x, y
+
+def get_sd(x: list, mean: float):
+    return stdev(x, mean)
+
 
 
 
