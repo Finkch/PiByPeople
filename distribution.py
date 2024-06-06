1de9239dd22d0094399fa56af1c48b800ddb9ce7
Finkch <skyler@thealdersons.org>
2024-06-05 21:09:45 -0700

    Added a smaller, more efficient distribution class

--- distribution.py
+++ distribution.py
@@ -2,9 +2,10 @@
 # the humans did at generating pi.
 
 from random_numbers import random, initialise_random
-from numpy import array, histogram
+from numpy import array, histogram, sqrt
 from dataset import PairList
-from math import floor
+from calculate import is_coprime
+from logger import logger
 
 
 # Generates lots of PairLists of random numbers to
@@ -25,7 +26,7 @@ class Distribution:
 
 
     # Creates the data for the distribution
-    def generate(self) -> None:
+    def generate(self):
 
         # Sets seed via system time (or OS specific random)
         initialise_random(None)        
@@ -49,6 +50,45 @@ class Distribution:
     # Returns a histogram of the pi data    
     def histogram(self) -> tuple:        
         return histogram(self.distribution[:,0])
+    
+
+# Runs much faster than regular distribution, but less useful.
+#   Used for generating histogram.
+class SmallDistribution:
+    def __init__(self, trials: int, length: int, max_num = 1e5) -> None:
+        self.trials = trials
+        self.length = length
+
+        self.distribution = self.generate()
+
+    def generate(self):
+        initialise_random(None)
+        dist = []
+
+        for i in range(self.trials):
+            
+            coprimes = sum([
+                is_coprime(
+                    [random(end=1e5), random(end=1e5)]
+                ) for j in range(self.length)
+            ])
+
+            dist.append(coprimes)
+
+
+
+        
+        pis = sqrt(6 / array(dist) * self.length)
+        
+        for i in range(self.trials):
+            logger.loga('copris', f'{i}:\t{dist[i]} --> {pis[i]}')
+
+        logger.loga('copris', '\n\n----------\n\n')
+
+        return pis
+    
+    def histogram(self) -> tuple:
+        return histogram(self.distribution)
 
         
         
