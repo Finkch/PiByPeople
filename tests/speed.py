fa398cef1bd848ed65f1341c9bc7256dd0b38496
Finkch <skyler@thealdersons.org>
2024-06-07 10:36:41 -0700

    Added test to determine which gcd_is_n is the fastest (they're all about the same

--- tests/speed.py
+++ tests/speed.py
@@ -2,7 +2,7 @@
 
 from distribution import *
 
-
+# Distribution tests
 def test_distributions(trials):
     args = [
         100,
@@ -10,6 +10,17 @@ def test_distributions(trials):
     ]
 
     return args, [create_dist, create_sdist, create_ssdist, create_sdist_trials, create_sdist_length], ['dist', 'sdist', 'ssdist', 'trials dist', 'length dist'], False
+
+
+# Generator tests
+def test_gcd_is_n(trials):
+    args = [
+        100,
+        100,
+        1e5
+    ]
+
+    return args, [gcdisn_small, gcdisn_rd, gcdisn_distrd], ['Small Dist', 'Random Dist', 'Random Dist Dist'], False
 
 
 '''     Distribution findings
@@ -36,3 +47,22 @@ def create_sdist_trials(trials, length):
 
 def create_sdist_length(trials, length):
     dist = SmallDistribution(trials, length * 10)
+
+
+
+
+'''     Generators findings
+
+Turns out they are all comperable to one another. There is only
+a ~5% difference between each of these implementations. That said,
+SmallDistribution is the fastest and RD of RDs is the slowest.
+
+'''
+def gcdisn_small(trials, length, max_num):
+    dist = SmallDistribution(trials, length, max_num)
+
+def gcdisn_rd(trials, length, max_num):
+    dist = RandomDistribution(dist_gcd_is_n, trials, max_num, length, 1)
+
+def gcdisn_distrd(trials, length, max_num):
+    dist = RandomDistribution(dist_dist_gcd_is_n, trials, max_num, length, 1)
