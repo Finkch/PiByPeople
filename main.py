af5d9cab7f160365092490c8166ef32f133bf932
Finkch <skyler@thealdersons.org>
2024-06-07 12:04:13 -0700

    Updated main to reflect recent changes

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 # Welcome to PiByPeople!
 # This is the main file. It get the ball rolling!
 
+# TODO:
+#   Test performance of double gcd == 1
+#       Reimplement gross RandomDist of RandomDistss and add it to test
+
+
 from dataset import Dataset
 from distribution import *
-from show import print_pi, plot_distribution, plot
-
-from calculate import cfs
-
-from numpy import average
-
-from logger import logger
+from generators import *
+from guesses import *
+from show import plot
 
 
 def main():
@@ -18,28 +19,23 @@ def main():
 
     dataset = Dataset(f'data/{data_file}.csv')
 
-    # compare = SmallDistribution(1000, dataset.length * 1000)
 
-    # print_pi(dataset.live_random.pi)
-    # print_pi(dataset.semi_random.pi)
-    # print_pi(dataset.true_random.pi)
-    # print()
-
-    # print(print_pi(average(compare.distribution)))
-    # plot_distribution(compare)
-
-
-    dist = RandomDistribution(factors, 10000, 1e5)
+    # dist = RandomDistribution(factors, 10000, 1e5)
     # dist = RandomDistribution(common_factors, 10000, 1e100)
     # dist = RandomDistribution(greatest_common_denominator, 10000, 1e100)
-    # dist = RandomDistribution(gcd_is_n, 10000, 1e100, 3)
-    # dist = RandomDistribution(dist_gcd_is_n, 1000, 1e3, dataset.length * 10, 1)
+    # dist = RandomDistribution(gcd_is_n, 10000, 1e100, 1)
+
+    # dist = RandomDistribution(dist_gcd_is_n, 100, 1e3, dataset.length, 1)
+    # dist = RandomDistribution(dist_dist_gcd_is_n, 100, 1e3, dataset.length, 1, final_step = lambda d: ([sqrt(6 / (d.y / d.args[1]))], [1]))
+    
+    dist = RandomDistribution(dist_pi, 10000, 1e3, dataset.length)
     
 
-    dist.guess(inverse, 'Inverse', (dist.trials, 1, -1))
-    dist.guess(inverse_log, 'Inverse Logarithm', (dist.trials, 1, -1))
+    # dist.guess(inverse, 'Inverse', (dist.trials, 1, -1))
+    # dist.guess(inverse_log, 'Inverse Logarithm', (dist.trials, 1, -1))
 
     plot(dist)
+
 
 
 
