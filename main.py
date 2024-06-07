4e1eb5dd3314ae3357220fe686c37c30c74d8548
Finkch <skyler@thealdersons.org>
2024-06-06 23:13:42 -0700

    Updated main to reflect recent changes

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from dataset import Dataset
 from distribution import *
 from show import print_pi, plot_distribution, plot
 
+from calculate import cfs
+
 from numpy import average
 
 from logger import logger
@@ -12,9 +14,9 @@ from logger import logger
 
 def main():
     
-    # data_file = '2018'
+    data_file = '2018'
 
-    # dataset = Dataset(f'data/{data_file}.csv')
+    dataset = Dataset(f'data/{data_file}.csv')
 
     # compare = SmallDistribution(1000, dataset.length * 1000)
 
@@ -27,13 +29,18 @@ def main():
     # plot_distribution(compare)
 
 
-
-    dist = RandomDistribution(factors, 1000, 1e4)
+    dist = RandomDistribution(factors, 10000, 1e5)
+    # dist = RandomDistribution(common_factors, 10000, 1e100)
+    # dist = RandomDistribution(greatest_common_denominator, 10000, 1e100)
+    # dist = RandomDistribution(gcd_is_n, 10000, 1e100, 3)
+    # dist = RandomDistribution(dist_gcd_is_n, 1000, 1e3, dataset.length * 10, 1)
+    
 
     dist.guess(inverse, 'Inverse', (dist.trials, 1, -1))
     dist.guess(inverse_log, 'Inverse Logarithm', (dist.trials, 1, -1))
 
     plot(dist)
+
 
 
 
