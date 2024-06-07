c854d3852c90a73860aa1303d071d8b3b84bd9e3
Finkch <skyler@thealdersons.org>
2024-06-07 16:50:07 -0700

    main() now runs the full test! Just about done

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from dataset import Dataset
 from distribution import *
 from generators import *
 from guesses import *
-from show import plot
+from show import plot_pi
 import numpy as np
 
 def main():
@@ -24,14 +24,21 @@ def main():
 
     # Creates the distribution of π guesses
     dist = RandomDistribution(dist_pi, trials, max_num, dataset.length)
-    dist.normalise() # Normalise!
+
+    # Gets the percentile in which human π falls.
+    #   This has to be called before normalisation.
+    # score = dist.bottom_percent(dataset.live_random.pi, np.pi)
+    score = dist.bottom_percent(dataset.live_random.pi, np.pi)
+
+    # Normalise the distribution
+    dist.normalise() 
 
     # Adds some guess curves for the underlying distribution
     dist.guess(normal, 'Normal', (np.pi, 0.25))
     dist.guess(log_nonormal, 'Log-Normal', (2, 0.25, 2))
 
     # Shows the results
-    plot(dist, dataset, title='$\pi$ by People', axes=('$\pi$', 'counts (normalised)'))
+    plot_pi(dist, dataset, score, title='$\pi$ by People', axes=('$\pi$', 'counts (normalised)'))
 
 
 
