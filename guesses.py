455f882fba107d36d396fd915305a6c99974f0fc
Finkch <skyler@thealdersons.org>
2024-06-07 13:32:08 -0700

    Added more non-normalised guesses

--- guesses.py
+++ guesses.py
@@ -1,7 +1,7 @@
 # Functions used to guess at the underlying distribution
 
 import numpy as np
-from scipy.special import factorial
+from scipy.special import factorial, gamma as gammaf
 
 
 # An inverse with plenty of arguments
@@ -20,14 +20,22 @@ def poisson(x, l, a):
 def inverse_log(x, a, b, c):
     return a / np.log(x + b) + c
 
-# A normal distribution, unnormalised
-def nonnormal(x, m, s, a):
-    return a * np.e ** (-0.5 * ((x - m) / s) ** 2)
-
 # A normal distribution, normalised
 def normal(x, m, s):
     return (1  / s / np.sqrt(2 * np.pi)) * np.e ** (-0.5 * ((x - m) / s) ** 2)
 
-# A log-normal
-def log_normal(x, m, s, a):
-    return a * np.e ** (-0.5 * (np.log(x - m) / s) ** 2)
+# A normal distribution, unnormalised
+def nonnormal(x, m, s, a):
+    return a * np.e ** (-0.5 * ((x - m) / s) ** 2)
+
+# Log-normal
+def log_normal(x, m, s):
+    return 1 / (x * s * np.sqrt(2 * np.pi)) * np.e ** (-0.5 * (np.log(x - m) / s) ** 2)
+
+# Log-normal, unnormalised
+def log_nonormal(x, m, s, a):
+    return a / x * np.e ** (-0.5 * (np.log(x - m) / s) ** 2)
+
+# Chi squared
+def chi_squared(x, k, a):
+    return a * x ** (k / 2 - 1) * np.e ** (-x / 2)
