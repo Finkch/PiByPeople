2f93fdb4758fca3c4b9492548eaa2a73f0254c11
Finkch <skyler@thealdersons.org>
2024-06-09 17:03:44 -0700

    Added updateCI() to log confidence interval

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from generators import *
 from guesses import *
 from show import plot_pi, plot, display
 import numpy as np
+from logger import logger
 
 # Finds what π is, through human generated values
 def piByPeople():
@@ -17,7 +18,7 @@ def piByPeople():
     pi_by_people = PiDistribution(human_pi, None, data_file)
 
     # Number of trails
-    trials = int(1e4)
+    trials = int(1e3)
 
     # Maximum size of the random number
     max_num = int(1e9)
@@ -66,9 +67,33 @@ def testDistributions():
     display(save = False)
 
 
+# For updates the confidence interval for automatic updates
+def updateCI():
+    
+    trials = int(1e6)
+
+    # Gets human pi
+    pi_by_people = PiDistribution(human_pi, None, '2018')
+
+    dist = RandomDistribution(
+        dist_pi,            # The distribution
+        trials,             # Trials to perform
+        int(1e30),          # Max size of random numbers
+        pi_by_people.x[0]   # Count of number pairs
+    )
+
+    # Finds the percentile in which human π falls
+    score = dist.bottom_percent(pi_by_people.pi, np.pi)
+
+    # Logs the score for future reference
+    logger.loga('ci', f'{score}')
+
+
 # Ensures only the startup thread runs main
 if __name__ == '__main__':
     
-    piByPeople()
+    # piByPeople()
 
     # testDistributions()
+
+    updateCI()
